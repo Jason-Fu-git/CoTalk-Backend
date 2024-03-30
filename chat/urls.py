61551695ffa81e85b535506573a4b80c990f9112
Jason Fu <2022012206@admin.secoder.net>
2024-03-30 19:07:38 +0800

    delete(chat-websocket): 将原先chat中的websocket demo删除，主要功能已由ws实现

--- chat/urls.py
+++ chat/urls.py
@@ -2,7 +2,3 @@ from django.urls import path
 from . import views
 
 app_name = 'chat'
-
-urlpatterns = [
-    path('room/', views.chat_room, name='chat_room'),
-]
--- chat/views.py
+++ chat/views.py
@@ -1,6 +1,3 @@
 from django.shortcuts import render
 
 # Create your views here.
-
-def chat_room(request):
-    return render(request, 'chat/room.html')
